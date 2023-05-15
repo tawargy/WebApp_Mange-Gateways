bfd645437688d85c05f285dd3f286f2ea1338f36
Mohamed El Tawargy <tawargy.dev@gmail.com>
2023-05-15 14:56:15 +0300

    add  gateway and petipheral types

---
+++ server/types.ts
@@ -0,0 +1,14 @@
+import { Types } from 'mongoose';
+export interface Gateway{
+  serial:string,
+  name:string,
+  ip:string,
+  peripherals:Types.ObjectId[]
+}
+
+export interface Peripheral{
+  uid:number,
+  vendor:string,
+  status:string,
+  gatewayId:Types.ObjectId
+}
