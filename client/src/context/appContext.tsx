28a6254b1085bcdffbdaeefd9e8f01f5e88b44c3
Mohamed El Tawargy <tawargy.dev@gmail.com>
2023-07-12 22:49:49 +0300

    enhance performance

--- client/src/context/appContext.tsx
+++ client/src/context/appContext.tsx
@@ -1,82 +1,104 @@
-import React, {createContext, useContext, useState} from 'react'
-import {GatewayType, PeripheralType} from '../types'
+import React, { createContext, useContext, useState, useMemo } from 'react';
+import { GatewayType, PeripheralType } from '../types';
 
 interface State {
-  gateways: GatewayType[] | undefined
-  setGateways: React.Dispatch<React.SetStateAction<GatewayType[] | undefined>>
-  gateway: GatewayType | undefined
-  setGateway: React.Dispatch<React.SetStateAction<GatewayType | undefined>>
+  gateways: GatewayType[] | undefined;
+  setGateways: React.Dispatch<React.SetStateAction<GatewayType[] | undefined>>;
+  gateway: GatewayType | undefined;
+  setGateway: React.Dispatch<React.SetStateAction<GatewayType | undefined>>;
 
-  peripherals: PeripheralType[] | undefined
+  peripherals: PeripheralType[] | undefined;
   setPeripherals: React.Dispatch<
     React.SetStateAction<PeripheralType[] | undefined>
-  >
-  peripheral: PeripheralType | undefined
+  >;
+  peripheral: PeripheralType | undefined;
   setPeripheral: React.Dispatch<
     React.SetStateAction<PeripheralType | undefined>
-  >
-  isGatewayAddMode: boolean
-  setIsGatewayAddMode: React.Dispatch<React.SetStateAction<boolean>>
-  isGatewayEditMode: boolean
-  setIsGatewayEditMode: React.Dispatch<React.SetStateAction<boolean>>
+  >;
+  isGatewayAddMode: boolean;
+  setIsGatewayAddMode: React.Dispatch<React.SetStateAction<boolean>>;
+  isGatewayEditMode: boolean;
+  setIsGatewayEditMode: React.Dispatch<React.SetStateAction<boolean>>;
 
-  isPerAddMode: boolean
-  setIsPerAddMode: React.Dispatch<React.SetStateAction<boolean>>
-  isPerEditMode: boolean
-  setIsPerEditMode: React.Dispatch<React.SetStateAction<boolean>>
+  isPerAddMode: boolean;
+  setIsPerAddMode: React.Dispatch<React.SetStateAction<boolean>>;
+  isPerEditMode: boolean;
+  setIsPerEditMode: React.Dispatch<React.SetStateAction<boolean>>;
 
-  error: string | undefined
-  addError:(message:string)=>void
+  error: string | undefined;
+  addError: (message: string) => void;
 }
 
-const AppContext = createContext<State>({} as State)
+const AppContext = createContext<State>({} as State);
 
-const AppProvider = ({children}: {children: React.ReactNode}) => {
-  const [gateways, setGateways] = useState<GatewayType[] | undefined>()
-  const [gateway, setGateway] = useState<GatewayType | undefined>()
-  const [peripherals, setPeripherals] = useState<PeripheralType[] | undefined>()
-  const [peripheral, setPeripheral] = useState<PeripheralType | undefined>()
-  const [isGatewayAddMode, setIsGatewayAddMode] = useState(false)
-  const [isGatewayEditMode, setIsGatewayEditMode] = useState(false)
-  const [isPerAddMode, setIsPerAddMode] = useState(false)
-  const [isPerEditMode, setIsPerEditMode] = useState(false)
-  const [error, setError] = useState('')
+function AppProvider({ children }: { children: React.ReactNode }) {
+  const [gateways, setGateways] = useState<GatewayType[] | undefined>();
+  const [gateway, setGateway] = useState<GatewayType | undefined>();
+  const [peripherals, setPeripherals] = useState<
+    PeripheralType[] | undefined
+  >();
+  const [peripheral, setPeripheral] = useState<PeripheralType | undefined>();
+  const [isGatewayAddMode, setIsGatewayAddMode] = useState(false);
+  const [isGatewayEditMode, setIsGatewayEditMode] = useState(false);
+  const [isPerAddMode, setIsPerAddMode] = useState(false);
+  const [isPerEditMode, setIsPerEditMode] = useState(false);
+  const [error, setError] = useState('');
 
   const addError = (message: string) => {
-    setError(message)
+    setError(message);
     setTimeout(() => {
-      setError('')
-    }, 4000)
-  }
+      setError('');
+    }, 4000);
+  };
+
+  const memoizedValue = useMemo<State>(
+    () => ({
+      gateways,
+      setGateways,
+      gateway,
+      setGateway,
+      peripherals,
+      setPeripherals,
+      peripheral,
+      setPeripheral,
+      isGatewayAddMode,
+      setIsGatewayAddMode,
+      isGatewayEditMode,
+      setIsGatewayEditMode,
+      isPerAddMode,
+      setIsPerAddMode,
+      isPerEditMode,
+      setIsPerEditMode,
+      error,
+      addError,
+    }),
+    [
+      gateways,
+      setGateways,
+      gateway,
+      setGateway,
+      peripherals,
+      setPeripherals,
+      peripheral,
+      setPeripheral,
+      isGatewayAddMode,
+      setIsGatewayAddMode,
+      isGatewayEditMode,
+      setIsGatewayEditMode,
+      isPerAddMode,
+      setIsPerAddMode,
+      isPerEditMode,
+      setIsPerEditMode,
+      error,
+      addError,
+    ]
+  );
 
   return (
-    <AppContext.Provider
-      value={{
-        gateways,
-        setGateways,
-        gateway,
-        setGateway,
-        peripherals,
-        setPeripherals,
-        peripheral,
-        setPeripheral,
-        isGatewayAddMode,
-        setIsGatewayAddMode,
-        isGatewayEditMode,
-        setIsGatewayEditMode,
-        isPerAddMode,
-        setIsPerAddMode,
-        isPerEditMode,
-        setIsPerEditMode,
-        error,
-        addError,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  )
+    <AppContext.Provider value={memoizedValue}>{children}</AppContext.Provider>
+  );
 }
 
-export default AppProvider
+export default AppProvider;
 
-export const useAppContext = () => useContext(AppContext)
+export const useAppContext = () => useContext(AppContext);
